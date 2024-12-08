f5407887260b1c3506de798c17036d451fe0080a
Andranik Torgomyan <and.torgomyan01@gmail.com>
2024-12-08 20:12:36 +0400

    Fix bugs

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function $(target){
+  return document.querySelectorAll(target);
+}
+
+function $el(target){
+  return document.querySelector(target);
+}
+
+
+const searchFormInp = $('.search-form-inp');
+
+searchFormInp.forEach((input) => {
+  input.querySelector('input').addEventListener('focus', e => {
+    input.classList.add('focus');
+  })
+  input.querySelector('input').addEventListener('blur', e => {
+    input.classList.remove('focus');
+  })
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./*.html"], // HTML ֆայլերի ուղիները
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        'grey': '#11181C',
+        'blue': '#1150CB',
+      },
+    },
   },
   plugins: [],
 };
