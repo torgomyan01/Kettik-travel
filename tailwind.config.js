bd58cae280418699d9c42989dbb298dfb9a5126d
Andranik Torgomyan <and.torgomyan01@gmail.com>
2024-12-13 17:50:15 +0400

    End search result

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,45 @@ module.exports = {
         'grey': '#11181C',
         'blue': '#1150CB',
         'orange': '#FF7A00',
+        'purple': '#6A8AFF',
       },
       transitionDuration: {
         DEFAULT: '300ms',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      const flexClasses = {
+        '.flex-jsb-c': {
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        },
+        '.flex-js-c': {
+          display: 'flex',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+        },
+        '.flex-jc-c': {
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        '.flex-jse-c': {
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          alignItems: 'center',
+        },
+        '.flex-je-c': {
+          display: 'flex',
+          justifyContent: 'e',
+          alignItems: 'center',
+        },
+        // Կարող եք ավելացնել ավելի շատ դասեր ըստ պահանջի
+      };
+
+      addComponents(flexClasses);
+    },
+  ],
 };
